eb58da615fa07a4925b90121a89ed397d225db20
Manuel Velasco <manuel.velascosuarez@outlook.com>
2019-03-03 20:15:42 +0100

    Implemented the type evaluation of declarations and list of declarations (a program)

---
+++ src/Sharade/Semantic/Internal/Utils.hs
@@ -0,0 +1,11 @@
+module Sharade.Semantic.Internal.Utils where
+
+  import Data.List
+
+  allUnique :: (Ord a) => [a] -> Bool
+  allUnique = allUnique' . sort where
+    allUnique' [] = True
+    allUnique' [_] = True
+    allUnique' (x:y:xs)
+      | x == y = False
+      | otherwise = allUnique' (y:xs)
--- src/Sharade/Semantic/Transform.hs
+++ src/Sharade/Semantic/Transform.hs
@@ -5,19 +5,19 @@ module Sharing.Semantic.Transform where
   
   import Debug.Trace
 
+  import Sharade.Semantic.Internal.Utils
   import Sharade.Parser.Syntax
   import Sharade.Semantic.Semantic
   
   type Scope = Map.Map String Type
 
   boolType = SType "Bool"
-  integerType = SType "Integer"
-  doubleType = SType "Double"
+  numberType = SType "Number"
 
   typeOfLiteral :: String -> Type
   typeOfLiteral "True" = boolType
   typeOfLiteral "False" = boolType
-  typeOfLiteral _ = doubleType
+  typeOfLiteral _ = numberType
 
   typeOf :: TExpr -> Type
   typeOf (TLit t _) = t
@@ -75,5 +75,65 @@ module Sharing.Semantic.Transform where
     
     (lte, scope2) <- evaluateType le scope1 (FType (typeOf rte) expected)
 
-    let (FType _ t) = typeOf lte
+    let (FType t1 t) = typeOf lte
+
+    -- Maybe its neccesary to re-evaluate the right expression, if the types are
+    -- not equal (Probably the case when rte is Any).
+    (rte, scope2) <- if t1 /= (typeOf rte)
+      then evaluateType re scope2 t1
+      else return (lte, scope2)
+
     return (TFun t lte rte, scope2)
+  
+  translateDecl :: FDecl -> Scope -> Maybe TFDecl
+  translateDecl (FDecl fname fargs e) globalScope = do
+    -- Create the local scope, adding all the arguments
+    let lscope = foldl (\scope y -> Map.insert y Any scope) globalScope fargs
+
+    (te, lscope2) <- evaluateType e lscope Any
+
+    -- Form the final function type
+    let ftype = foldr (\x y -> FType (Map.findWithDefault Any x lscope2) y) (typeOf te) fargs
+    
+    return (TFDecl ftype fname fargs te)
+  
+  initGlobalScope :: Scope
+  initGlobalScope = Map.fromList [
+    ("(+)",  FType numberType (FType numberType numberType)) ]
+    --("(-)",  FType numberType (FType numberType numberType))),
+    --("(*)",  FType numberType (FType numberType numberType))),
+    --("(/)",  FType numberType (FType numberType numberType))),
+    --("(==)", FType numberType (FType numberType boolType))),
+    --("(!=)", FType numberType (FType numberType boolType)))),
+    --("(<)",  FType numberType (FType numberType boolType)))),
+    --("(<=)", FType numberType (FType numberType boolType)))),
+    --("(>)",  FType numberType (FType numberType boolType)))),
+    --("(>=)", FType numberType (FType numberType boolType)))),
+  --] 
+  
+  createBasicFType :: FDecl -> Maybe Type
+  createBasicFType (FDecl fname fargs _) = do
+    -- A function is well-defined if the name of the function does not appear on
+    -- its arguments and if two arguments are not equal.
+    guard (allUnique (fname:fargs))
+    
+    -- Form the function type full of any
+    return $ foldr (\_ y -> FType Any y) Any fargs
+  
+  funNames [] = []
+  funNames ((FDecl name _ _):fs) = name : funNames fs
+
+  createGlobalScope :: [FDecl] -> Maybe Scope
+  createGlobalScope fs = do
+    guard (allUnique $ funNames fs)
+
+    ts <- sequence $ map createBasicFType fs
+    let scope = Map.fromList $ zip (funNames fs) ts
+
+    return $ Map.union initGlobalScope scope
+
+  translateProgram :: [FDecl] -> Maybe [TFDecl]
+  translateProgram fs = do
+    globalScope <- createGlobalScope fs
+
+    sequence $ map (\x -> translateDecl x globalScope) fs
